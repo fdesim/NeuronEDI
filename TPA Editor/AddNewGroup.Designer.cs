66f3e038200a7b7596f3c61b6f28d19f1b9da581
Frank DeSimone <Frank.DeSimone@neudesic.com>
2017-08-10 13:17:38 -0500

    initisl check-in

--- TPA Editor/AddNewGroup.Designer.cs
+++ TPA Editor/AddNewGroup.Designer.cs
@@ -59,6 +59,7 @@
             this.buttonSave.TabIndex = 2;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // labelFunctionalGroupID
             // 
--- TPA Editor/AddNewGroup.cs
+++ TPA Editor/AddNewGroup.cs
@@ -16,5 +16,10 @@ namespace TPA_Editor
         {
             InitializeComponent();
         }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
